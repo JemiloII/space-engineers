9765c248c14a389cfd6ddecfdb9ac8d0276d7fe7
Brian Jemilo II <jemiloii@gmail.com>
2020-08-17 05:54:27 -0500

    Remove debug code

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = require('./lib/app');
 const server = require('./lib/server');
 const session = require('./lib/session');
 
-session.players('76561198047446267', {searchKey: 'SteamID'})
-  .then(console.log)
-  .catch(console.error);
-
 module.exports = {
   admin,
   api,
